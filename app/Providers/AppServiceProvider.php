b5d367726d380d457dba7e4f9e26d15219e8d70f
hina <hina@hinaloe.net>
2019-10-12 20:59:03 +0900

    Ignore force-https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        // URL::forceScheme('https');
         Schema::defaultStringLength(191);
 
         Avatar::observe(AvatarObserver::class);
